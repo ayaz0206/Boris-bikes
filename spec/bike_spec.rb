2fb1547bc3bb2ed4a718aca17767d710a727f0ea
Ayazg <ayaz0206@gmail.com>
2014-01-20 15:46:56 +0000

    Implemented broken method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,4 +7,13 @@ describe Bike do
 		bike = Bike.new
 		expect(bike).not_to be_broken
 	end
+
+
+	it "should be able to break" do 
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
+	end
+
 end
+
